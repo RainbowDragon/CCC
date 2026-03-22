c54aaaefcdcb0742e5ffc2ed8bf3379155fa2ca6
Xiang Xuan <xuanxiang@hotmail.com>
2026-03-21 21:56:34 -0700

    CCC 2026 Senior - Beams of Light

---
+++ CPlusPlus/Senior/2026/BeamsOfLight.cpp
@@ -0,0 +1,52 @@
+/**
+ *      CCC 2026 Senior - Beams of Light
+ *
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int N, L, Q;
+    cin >> N;
+    cin >> L;
+    cin >> Q;
+
+    int diff[N+2];
+    memset(diff, 0, sizeof(diff));
+    
+    for (int i = 0; i < L; i++)
+    {
+        int P, S;
+        cin >> P >> S;
+        int lb = max(1, P - S);
+        int rb = min(N, P + S);
+        diff[lb] += 1;
+        diff[rb+1] -= 1;
+    }
+
+    int parking[N+1];
+    parking[0] = 0;
+    for (int i = 1; i <= N; i++)
+    {
+        parking[i] = parking[i-1] + diff[i];
+    }
+
+    for (int i = 0; i < Q; i++)
+    {
+        int pos;
+        cin >> pos;
+        if (1 <= pos && pos <= N && parking[pos] > 0)
+        {
+            cout << "Y" << endl;
+        }
+        else 
+        {
+            cout << "N" << endl;
+        }
+    }
+
+    return 0;
+}
